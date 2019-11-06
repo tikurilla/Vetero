1d2a76e2f0abb727dba0b66b42cb61cecd016e55
Timur Golovnya <tgolovnya@smart-consulting.ru>
2019-11-06 17:42:45 +0700

    initial commit

---
+++ vetero-server/src/main/java/com/vetero/veteroserver/VeteroServerApplication.java
@@ -0,0 +1,13 @@
+package com.vetero.veteroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteroServerApplication.class, args);
+	}
+
+}
---
+++ vetero-server/src/test/java/com/vetero/veteroserver/VeteroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetero.veteroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
